4c3f6ddc2c6e35290a2e167f18d49fa68b8e6820
Ayoub Ghozzi <33912575+ghozziayoub@users.noreply.github.com>
2025-12-11 15:45:11 +0100

    fix deploy

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient({
-  datasourceUrl: process.env.PRISMA_DATABASE_URL
+  accelerateUrl: process.env.DATABASE_URL
 }).$extends(withAccelerate());
 
 const app = express();
